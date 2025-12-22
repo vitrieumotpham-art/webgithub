20ee7e5233d197d55789373ffda7de1cb4661f25
vitrieumotpham-art <vitrieumot.pham@gmail.com>
2025-12-23 01:10:42 +0700

    Fix path and case sensitivity for controllers

--- routes/admin/dashboard.routes.js
+++ routes/admin/dashboard.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const route=express.Router();
 
-const controller=require("../../../management_products/controllers/admin/dashboard.controller");
+const controller = require("../../controllers/admin/dashboard.controller");
 
 route.get("/",controller.dashboard);
 
