99fe90b88cceb659c982104beefba2f41b669893
vitrieumotpham-art <vitrieumot.pham@gmail.com>
2025-12-23 01:54:57 +0700

    Fix case sensitivity and view path for project

--- config/database.js
+++ config/database.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 
-module.exports.connect=async= ()=>{
+module.exports.connect = async () => {
     try {
-        mongoose.connect(process.env.MONGODB_URL);
-        console.log("connect success!")
+        // Thêm await để đợi kết nối thành công mới chạy tiếp các lệnh sau
+        await mongoose.connect(process.env.MONGODB_URL, {
+    maxPoolSize: 10, // Duy trì tối đa 10 kết nối đồng thời
+    serverSelectionTimeoutMS: 5000, // Thử kết nối trong 5s, nếu không được thì báo lỗi ngay
+    socketTimeoutMS: 45000, // Đóng kết nối nếu không có phản hồi sau 45s
+});
+        console.log("Connect database success!");
     } catch (error) {
-          console.log("connect error!")
+        console.log("Connect database error!");
+        console.log(error);
     }
 }
