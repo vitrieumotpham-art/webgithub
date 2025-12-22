0f8ace6ce1365ca54b46ccea04240988411e2481
vitrieumotpham-art <vitrieumot.pham@gmail.com>
2025-12-23 01:18:37 +0700

    Fix all relative paths in client routers

--- routes/client/home.routers.js
+++ routes/client/home.routers.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const route=express.Router();
+const route = express.Router();
 
-const home=require("../../../management_products/controllers/client/home.controller");
-const about_us=require("../../../management_products/controllers/client/about_us");
-const duan=require("../../../management_products/controllers/client/duan");
-const khach=require("../../../management_products/controllers/client/khach");
-const lienhe=require("../../../management_products/controllers/client/lienhe");
-const tintuc=require("../../../management_products/controllers/client/tintuc");
-const dichvu=require("../../../management_products/controllers/client/dichvu");
-route.get("/",home.home);
-route.get("/about_us",about_us.chungtoi);
-route.get("/duan",duan.duan);
-route.get("/khach",khach.khach);
-route.get("/lienhe",lienhe.lienhe);
-route.get("/tintuc",tintuc.tintuc);
-route.get("/dichvu",dichvu.dichvu);
-module.exports=route;   
+// Sửa lại đường dẫn lùi 2 cấp (../../) để vào thẳng thư mục controllers
+const home = require("../../controllers/client/home.controller");
+const about_us = require("../../controllers/client/about_us");
+const duan = require("../../controllers/client/duan");
+const khach = require("../../controllers/client/khach");
+const lienhe = require("../../controllers/client/lienhe");
+const tintuc = require("../../controllers/client/tintuc");
+const dichvu = require("../../controllers/client/dichvu");
+
+route.get("/", home.home);
+route.get("/about_us", about_us.chungtoi);
+route.get("/duan", duan.duan);
+route.get("/khach", khach.khach);
+route.get("/lienhe", lienhe.lienhe);
+route.get("/tintuc", tintuc.tintuc);
+route.get("/dichvu", dichvu.dichvu);
+
+module.exports = route;
